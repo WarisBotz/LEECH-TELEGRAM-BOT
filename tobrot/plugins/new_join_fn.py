bca4670ab003b9e088ae766638bcef3b656bd278
Avi Patil <58633270+proavipatil@users.noreply.github.com>
2021-08-08 21:41:17 +0530

    Update new_join_fn.py

--- tobrot/plugins/new_join_fn.py
+++ tobrot/plugins/new_join_fn.py
@@ -15,7 +15,7 @@ async def new_join_f(client, message):
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton('Channel', url='https://t.me/MaxxBots')
+                        InlineKeyboardButton('Channel', url='https://t.me/WARISZONE')
                     ]
                 ]
                )
